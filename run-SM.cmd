76fac6762251a539e907a246a10ac6f322a2e35f
anjisuan608 <anjisuan608@petalmail.com>
2025-04-03 23:45:02 +0800

    修复了authlib下载完成后的跳转错误

--- run-SM.cmd
+++ run-SM.cmd
@@ -915,9 +915,9 @@ echo 键入"l"使用certutil方案从官方下载源下载(适用于PowerShell不可用时)
 echo 键入"u"返回上一级菜单
 echo 键入"x"退出批处理
 choice /C oblux /CS
-if %errorlevel% == 1 powershell -Command "Invoke-WebRequest -Uri https://bmclapi2.bangbang93.com/mirrors/authlib-injector/artifact/53/authlib-injector-1.2.5.jar -OutFile .\authlib-injector-1.2.5.jar" && goto CheckAuthJar
-if %errorlevel% == 2 powershell -Command "Invoke-WebRequest -Uri https://authlib-injector.yushi.moe/artifact/53/authlib-injector-1.2.5.jar -OutFile .\authlib-injector-1.2.5.jar" && goto CheckAuthJar
-if %errorlevel% == 3 certutil -urlcache -split -f https://authlib-injector.yushi.moe/artifact/53/authlib-injector-1.2.5.jar .\authlib-injector-1.2.5.jar && goto CheckAuthJar
+if %errorlevel% == 1 powershell -Command "Invoke-WebRequest -Uri https://bmclapi2.bangbang93.com/mirrors/authlib-injector/artifact/53/authlib-injector-1.2.5.jar -OutFile .\authlib-injector-1.2.5.jar" && goto AuthLibCheck
+if %errorlevel% == 2 powershell -Command "Invoke-WebRequest -Uri https://authlib-injector.yushi.moe/artifact/53/authlib-injector-1.2.5.jar -OutFile .\authlib-injector-1.2.5.jar" && goto AuthLibCheck
+if %errorlevel% == 3 certutil -urlcache -split -f https://authlib-injector.yushi.moe/artifact/53/authlib-injector-1.2.5.jar .\authlib-injector-1.2.5.jar && goto AuthLibCheck
 if %errorlevel% == 4 goto bc
 if %errorlevel% == 5 goto x
 color %colorError%
